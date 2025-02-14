46e3eee1ab61f6b0893bdc50712a9537e541f688
Rungaroon2005 <rungaroon2548@gmail.com>
2025-02-14 13:04:24 +0700

    Fix redline

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,14 +3,15 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World';
+    return 'Hello Git & GitHub';
   }
+
   getGit2(): string {
     return 'git  and github using xxx';
   }
   getJson3() {
-    return{
-      name:'Rungaroon'
-    }
+    return {
+      name: 'Rungaroon',
+    };
   }
 }
